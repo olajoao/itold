47bb0a57a8751d3b22bafe16399b06d99e180125
João Küster <kusterzito@gmail.com>
2023-08-14 18:11:01 -0300

    fix: hero section text color and spacing

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -3,26 +3,32 @@
     <div class="container mx-auto">
       <div class="h-full px-5 py-4 md:py-14">
         <h1
-          class="text-gray-800 dark:text-neutral-200 font-bold text-xl md:text-6xl"
+          class="mb-4 text-gray-800 dark:text-neutral-200 font-bold text-xl md:text-6xl"
+        >
+          Olá criticante,
+        </h1>
+        <p class="text-gray-700 dark:text-neutral-300">
+          Tá na dúvida de assistir
+          <span class="bg-emerald-100 dark:bg-emerald-500/20 px-1"
+            >aquele filme</span
+          >?
+        </p>
+        <p class="text-gray-700 dark:text-neutral-300">
+          Quer saber o que os outros acharam dele?
+        </p>
+        <p class="text-gray-700 dark:text-neutral-300">
+          Assistiu um filme maravilhoso e precisa contar para todo mundo?
+        </p>
+
+        <p class="mt-4 font-bold text-neutral-700 dark:text-neutral-300">
+          Seja muito bem vindo!
+        </p>
+
+        <h1
+          class="mt-14 text-gray-800 dark:text-neutral-200 font-bold text-xl md:text-4xl"
         >
           Populares agora
         </h1>
-        <p class="mt-2 xl:mt-8 text-gray-700 dark:text-neutral-300">
-          Tá na dúvida de assistir
-          <span class="bg-emerald-100 px-1">aquele filme</span>?
-        </p>
-        <p class="mt-2 xl:mt-8 text-gray-700 dark:text-neutral-300">
-          Quer saber o que os outros acharam dele?
-        </p>
-        <p class="mt-2 xl:mt-8 text-gray-700 dark:text-neutral-300">
-          Assistiu um filme maravilhoso e precisa contar pra todo mundo?
-        </p>
-
-        <p
-          class="font-bold mt-2 xl:mt-8 text-neutral-700 dark:text-neutral-300"
-        >
-          Seja muito bem vindo!
-        </p>
         <PopularMovieList />
       </div>
     </div>
